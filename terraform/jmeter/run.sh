5d5705fec62e4a2a548eb1b015b84f9dfba207ba
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2024-02-06 21:08:34 -0500

    actualizando jmeter

--- terraform/jmeter/run.sh
+++ terraform/jmeter/run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
 apt-get upgrade -y
+
+apt-get install -y gcc default-jre curl unzip
+
+curl https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.6.3.zip -o /opt/apache-jmeter-5.6.3.zip
+cd /opt
+unzip /opt/apache-jmeter-5.6.3.zip
+
+ls -la
